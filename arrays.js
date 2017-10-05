8e7f7bdd4f3abd41804f65762ad6e8e0079f5a75
Anu Jose <anujose1989@gmail.com>
2017-10-05 19:45:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,24 +5,35 @@ function addElementToBeginningOfArray(array,element){
    a.unshift(element);
    return a;
  }
-addElementToBeginningOfArray(chocolateBars, 'foo')
+addElementToBeginningOfArray(chocolateBars, 'foo');
 
 function addElementToEndOfArray(array,element){
   var a = array;
   a.push(element);
   return a;
 }
-addElementToEndOfArray(chocolateBars, 'foo')
+addElementToEndOfArray(chocolateBars, 'foo');
 
 
 function destructivelyAddElementToBeginningOfArray(array,element){
   array.unshift(element);
   return array;
 }
-destructivelyAddElementToBeginningOfArray(array,"foo")
+destructivelyAddElementToBeginningOfArray(array,"foo");
 
   function destructivelyAddElementToEndOfArray(array,element){
-    array.push("foo");
+    array.push(element);
     return array;
   }
-  destructivelyAddElementToEndOfArray(array,"foo")
+  destructivelyAddElementToEndOfArray(array,"foo");
+
+  function accessElementInArray (array, index){
+    return array[index];
+  }
+  accessElementInArray([1, 2, 3],2);
+
+  function destructivelyRemoveElementFromBeginningOfArray(array){
+    array.shift();
+    return array;
+  }
+  destructivelyRemoveElementFromBeginningOfArray([1, 2, 3]);
